ea31e7ff2cb2f6f84ef9f9a4368f547d56656a9c
KyoheiKinami <aiuekk@gmail.com>
2018-09-08 11:41:57 +0900

    dp8

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,14 @@
 set :branch, 'master'
 
-role :app, %w{apuser@118.27.20.28}
-role :web, %w{apuser@118.27.20.28}
-role :db,  %w{apuser@118.27.20.28}
+#role :app, %w{apuser@118.27.20.28}
+#role :web, %w{apuser@118.27.20.28}
+#role :db,  %w{apuser@118.27.20.28}
 
 server '118.27.20.28', user: 'apuser', roles: %w{web app db}, port: 10022
 
-#set :ssh_options, keys: 'D:\Create\ruby\key\twitmovie\apuser/id_rsa'
 set :ssh_options, {
-    forward_agent: true,
-    auth_methods: %w(publickey),
-    port: 10022
+  port: 10022, #XXXにポート番号
+  keys: [File.expand_path('D:\Create\ruby\key\twitmovie\apuser/id_rsa')], #'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  forward_agent: true,
+  auth_methods: %w(publickey)
 }
