401ae7417ca8e4bad4a95ecb13243481adbf7d4a
KyoheiKinami <aiuekk@gmail.com>
2018-09-08 11:17:00 +0900

    dp7

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,4 +6,9 @@ role :db,  %w{apuser@118.27.20.28}
 
 server '118.27.20.28', user: 'apuser', roles: %w{web app db}, port: 10022
 
-set :ssh_options, keys: 'D:\Create\ruby\key\twitmovie\apuser/id_rsa'
+#set :ssh_options, keys: 'D:\Create\ruby\key\twitmovie\apuser/id_rsa'
+set :ssh_options, {
+    forward_agent: true,
+    auth_methods: %w(publickey),
+    port: 10022
+}
