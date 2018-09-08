fc3614ab167e234267972f9a603026020d42ab62
KyoheiKinami <aiuekk@gmail.com>
2018-09-08 11:13:50 +0900

    dp6

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ set :keep_releases, 5
 set :rbenv_type, :system
 set :rbenv_ruby, '2.3.3'#rubyのバージョン
 
+set :ssh_options, :port => "10022"
+
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
